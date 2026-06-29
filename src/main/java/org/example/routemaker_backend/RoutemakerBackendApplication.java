091ad91824102c49e6e5277c71d4939db86d9b2f
SYJ <peter4045123@naver.com>
2026-06-29 21:40:22 +0900

    feat: connect to docker

---
+++ src/main/java/org/example/routemaker_backend/RoutemakerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.routemaker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutemakerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutemakerBackendApplication.class, args);
+    }
+
+}
